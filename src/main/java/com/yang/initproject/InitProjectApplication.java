a8f024d846e6e18ac754a8c3e84337c3164edb30
xingxingkangkang <ruikang_Y@163.com>
2021-06-14 15:30:28 +0800

    init project

---
+++ src/main/java/com/yang/initproject/InitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yang.initproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yang/initproject/InitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yang.initproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
